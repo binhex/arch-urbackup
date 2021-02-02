1c834e7f5beed0dbc13c8977d4cc235bcec668c9
binhex <megalith01@gmail.com>
2021-02-02 14:00:18 +0000

    change extension for compiled package

--- build/root/custom.sh
+++ build/root/custom.sh
@@ -18,7 +18,5 @@ sed -r -i -e 's~-march=native\s?~~g' 'PKGBUILD'
 # compile package
 /usr/bin/makepkg
 
-ls -alR
-
 # install compiled package using pacman
-pacman -U ${aur_package}*.tar.xz --noconfirm
+pacman -U ${aur_package}*.tar.zst --noconfirm
