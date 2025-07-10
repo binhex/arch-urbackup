e026e504bebe860985ff32fd113403b8cc514f49
binhex <megalith01@gmail.com>
2025-07-10 10:15:23 +0100

    various path fixes for scripts

--- build/root/install.sh
+++ build/root/install.sh
@@ -115,7 +115,7 @@ EOF
 sed -i '/# PERMISSIONS_PLACEHOLDER/{
     s/# PERMISSIONS_PLACEHOLDER//g
     r /tmp/permissions_heredoc
-}' /usr/local/bin/init.sh
+}' /usr/local/bin/shell/docker/init.sh
 rm /tmp/permissions_heredoc
 
 # create file with contents of here doc, note EOF is NOT quoted to allow us to expand variables
@@ -134,7 +134,7 @@ EOF
 sed -i '/# CONFIG_PLACEHOLDER/{
     s/# CONFIG_PLACEHOLDER//g
     r /tmp/config_heredoc
-}' /usr/local/bin/init.sh
+}' /usr/local/bin/shell/docker/init.sh
 rm /tmp/config_heredoc
 
 # env vars
